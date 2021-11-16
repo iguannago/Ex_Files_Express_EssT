dd44814390db141176582cb8e622ae5581ee1045
david.crespo <david.crespo@hmcts.net>
2021-11-16 13:45:56 +0000

    add deps

--- Exercise Files/sample/routes/index.js
+++ Exercise Files/sample/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'This is Express', 
+    text: 'Express'
+  });
 });
 
 module.exports = router;
