52f427c76eff4e4673fcbb56074191d68d993d85
david.crespo <david.crespo@hmcts.net>
2021-11-17 22:42:55 +0000

    add some more routes

--- Exercise Files/sample/routes/users.js
+++ Exercise Files/sample/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var debug = require("debug")("sample:routes:users");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', (req, res) => {
+  debug(`new user(id=${req.body.id}) added`);
+  res.send(`new user(id=${req.body.id}) added`);
 });
 
 module.exports = router;
