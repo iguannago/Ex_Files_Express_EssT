1d7909453427a3bb3b5efd4ab89f74528e1d0c2b
david.crespo <david.crespo@hmcts.net>
2021-11-18 07:57:29 +0000

    add some more routes

--- Exercise Files/sample/app.js
+++ Exercise Files/sample/app.js
@@ -19,10 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', indexRouter);
--- Exercise Files/sample/routes/index.js
+++ Exercise Files/sample/routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'This is Express', 
-    text: 'Express'
+    title: 'Tennis club', 
+    text: 'Villanueva de la Serena'
   });
 });
 
