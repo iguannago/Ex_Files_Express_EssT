99c059eb607e05cdbe04386712adcc7736679744
david.crespo <david.crespo@hmcts.net>
2021-11-18 19:59:18 +0000

    improve routes with params

--- Exercise Files/sample/routes/users.js
+++ Exercise Files/sample/routes/users.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var debug = require("debug")("sample:routes:users");
+var debug = require('debug')('sample:routes:users');
+var data = require('../data/data.json');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  debug('get all users call ...');
+  res.render("users", {
+    title: "Users",
+    data: data,
+  });
+});
+
+router.get('/:id', (req, res) => {
+  debug(`get user(id=${req.params.id}) call ...`);
+  res.render('users', {
+    title: 'User',
+    data: data.find((e) => e.id == req.params.id)
+  });
 });
 
 router.post('/', (req, res) => {
-  debug(`new user(id=${req.body.id}) added`);
-  res.send(`new user(id=${req.body.id}) added`);
+  debug(`add user(id=${req.body.id}) call ...`);
+  newUser = {
+    id: req.body.id,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email
+  };
+  data.push(newUser);
+  res.json(data[data.length -1]);
 });
 
 module.exports = router;
